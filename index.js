9b281413d3b443a5b4da9e88251c4d1a92c5a52b
Scott Gress <scottmgress@gmail.com>
2017-03-13 16:30:00 -0500

    Updates for use w/ Sails 1.0
    * No more using private APIs -- reload controllers using `sails.reloadActions`, and ORM using `sails.hooks.orm.reload()`
    * Add reloading of helpers
    * Add reloading trigger for `config/routes.js`

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-// oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-// WARNING: THIS HOOK USES PRIVATE, UNDOCUMENTED APIs THAT COULD CHANGE AT ANY TIME
-// oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-// This hook uses an undocumented, private Sails core method in order to reload controllers without 
-// having to lower/re-lift an app. You should not copy or reuse that code (clearly marked below) in an 
-// app, because future releases of Sails--even patch releases--may cause it to stop functioning.
-// The private API usage used below will be replaced by a public method as soon as it is available.
-// In the meantime, enjoy, and as stated in the README, do not turn this hook on in production!
 var path = require('path');
+var _ = require('@sailshq/lodash');
 module.exports = function(sails) {
 
   return {
@@ -29,7 +22,9 @@ module.exports = function(sails) {
           path.resolve(sails.config.appPath,'api','controllers'),
           path.resolve(sails.config.appPath,'api','models'),
           path.resolve(sails.config.appPath,'api','services'),
-          path.resolve(sails.config.appPath,'config','locales')
+          path.resolve(sails.config.appPath,'api','helpers'),
+          path.resolve(sails.config.appPath,'config','locales'),
+          path.resolve(sails.config.appPath,'config','routes.js')
         ],
         overrideMigrateSetting: true,
         // Ignored paths, passed to anymatch
@@ -57,9 +52,11 @@ module.exports = function(sails) {
 
       var self = this;
 
+      var routesConfigPath = path.resolve(sails.config.appPath,'config','routes.js');
+
       // If the hook has been deactivated, or controllers is deactivated just return
-      if (!sails.config[this.configKey].active || !sails.hooks.controllers) {
-        sails.log.verbose("Autoreload hook deactivated.");
+      if (!sails.config[this.configKey].active) {
+        sails.log.verbose("sails-hook-autoreload: Autoreload hook deactivated.");
         return cb();
       }
 
@@ -75,63 +72,55 @@ module.exports = function(sails) {
         ignored: sails.config[this.configKey].ignored
       });
 
-      sails.log.verbose("Autoreload watching: ", sails.config[this.configKey].dirs);
+      sails.log.verbose("sails-hook-autoreload: Autoreload watching: ", sails.config[this.configKey].dirs);
 
       // Whenever something changes in those dirs, reload the ORM, controllers and blueprints.
       // Debounce the event handler so that it only fires after receiving all of the change
       // events.
-      watcher.on('all', sails.util.debounce(function(action, path, stats) {
+      watcher.on('all', _.debounce(function(action, watchedPath, stats) {
 
-        sails.log.verbose("Detected API change -- reloading controllers / models...");
+        sails.log.verbose("sails-hook-autoreload: Detected API change -- reloading controllers / models...");
 
-        // don't drop database
+        // Don't use the configured migration strategy if `overrideMigrateSetting` is true.
         sails.config.models.migrate = sails.config[self.configKey].overrideMigrateSetting ? 'alter' : sails.config.models.migrate;
 
-        //                    \│/  ╦ ╦╔═╗╦═╗╔╗╔╦╔╗╔╔═╗  \│/                  
-        //  ─────────────────── ─  ║║║╠═╣╠╦╝║║║║║║║║ ╦  ─ ───────────────────
-        //                    /│\  ╚╩╝╩ ╩╩╚═╝╚╝╩╝╚╝╚═╝  /│\                  
-        //  ┬ ┬┌┐┌┌┬┐┌─┐┌─┐┬ ┬┌┬┐┌─┐┌┐┌┌┬┐┌─┐┌┬┐  ┌─┐┌─┐┬┌─┐  ┬┌┐┌  ┬ ┬┌─┐┌─┐
-        //  │ ││││ │││ ││  │ ││││├┤ │││ │ ├┤  ││  ├─┤├─┘│└─┐  ││││  │ │└─┐├┤ 
-        //  └─┘┘└┘─┴┘└─┘└─┘└─┘┴ ┴└─┘┘└┘ ┴ └─┘─┴┘  ┴ ┴┴  ┴└─┘  ┴┘└┘  └─┘└─┘└─┘
-        //  The loadAndRegisterControllers method is a _private_ method of the
-        //  controllers hook, and should not be used in your app code.
-        //  It will be replaced here as soon as a public "reload" method is added
-        //  to the controllers hook.  But in the meantime it's okay because
-        //  you're not using this in production, right?
-        //  
         //  Reload controller middleware
-        sails.hooks.controllers.loadAndRegisterControllers(function() {
+        sails.reloadActions(function() {
 
-          // Wait for the ORM to reload
-          sails.once('hook:orm:reloaded', function() {
+          // Reload helpers
+          sails.hooks.helpers.reload(function() {
 
-            // Reload locales
-            if (sails.hooks.i18n) {
-              sails.hooks.i18n.initialize(function() {});
-            }
+            // Wait for the ORM to reload
+            sails.hooks.orm.reload(function() {
 
-            // Reload services
-            if (sails.hooks.services) {
-              sails.hooks.services.loadModules(function() {});
-            }
+              // Reload services
+              if (sails.hooks.services) {
+                sails.hooks.services.loadModules(function() {});
+              }
 
-            // Reload blueprints on controllers
-            if (sails.hooks.blueprints) {
-              sails.hooks.blueprints.extendControllerMiddleware();
-            }
+              // Unset all of the current routes from the `explicitRoutes` hash.
+              // This hash may include some routes added by hooks, so can't just wipe
+              // it entirely, but in case some route URLs changed we don't want
+              // the old ones hanging around.
+              sails.router.explicitRoutes = _.omit(sails.router.explicitRoutes, function(action, address) {
+                return !!sails.config.routes[address];
+              });
+              // Reload the config/routes.js file.
+              try {
+                // Remove the routes config file from the require cache.
+                delete require.cache[require.resolve(routesConfigPath)];
+                sails.config.routes = require(routesConfigPath).routes;
+              } catch (e) {
+                sails.log.verbose('sails-hook-autoreload: Could not reload `' + routesConfigPath + '`.');
+              }
 
-            // Flush router
-            sails.router.flush();
+              // Flush the router.
+              sails.config.routes = _.extend({}, sails.router.explicitRoutes, sails.config.routes);
+              sails.router.flush(sails.config.routes);
 
-            // Reload blueprints
-            if (sails.hooks.blueprints) {
-              sails.hooks.blueprints.bindShadowRoutes();
-            }
+            });
 
           });
-
-          // Reload ORM
-          sails.emit('hook:orm:reload');
 
         });
 
